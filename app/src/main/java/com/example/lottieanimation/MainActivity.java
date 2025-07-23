b55bb4f7743bc91f19173d54539c39f50390bf14
abrar-idrisi <abrarcoding3334@gmail.com>
2025-07-23 18:27:41 +0530

    this is my third commit

--- app/src/main/java/com/example/lottieanimation/MainActivity.java
+++ app/src/main/java/com/example/lottieanimation/MainActivity.java
@@ -1,18 +1,30 @@
 package com.example.lottieanimation;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Toast;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.RenderMode; // Required for setRenderMode
 
 public class MainActivity extends AppCompatActivity {
+
+    LottieAnimationView laView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void getdata(){
-        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+
+        // 1. Initialize the view
+        laView = findViewById(R.id.lottieAnimationView);
+
+
+
+        // If using assets folder:
+        laView.setAnimation("animationlop.json");
+
+        // 3. Render mode & play
+        laView.setRenderMode(RenderMode.SOFTWARE);
+        laView.playAnimation();
     }
 }
